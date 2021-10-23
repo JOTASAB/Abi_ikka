5c493ca5aeac6df425c0ae01a65079c499e4c238
JOTASAB <90125197+JOTASAB@users.noreply.github.com>
2021-10-23 20:29:35 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://imgur.com/a/Xk78sgW";
+  r_text[0] = "https://imgur.com/a/Xk78sgW.jpg";
     
     
     var i = Math.floor(1*Math.random())
